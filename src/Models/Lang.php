5c0c67f7b5856363c661834f998729be92746a46
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2023-03-18 14:26:30 +0330

    ability to get list of language

--- src/Models/Lang.php
+++ src/Models/Lang.php
@@ -8,4 +8,12 @@ class Lang extends Model
 {
     protected $guarded = [];
     protected $table = 'lc_langs';
+
+    /**
+     * Get all language in available in database
+     */
+    public static function getLanguageList(): array
+    {
+        return self::query()->select('lang')->groupBy('lang')->pluck('lang')->toArray();
+    }
 }
