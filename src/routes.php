abe249e0f11ff36db7761ff2b008a2aaa91b8ba8
Pedram Davoodi <pedramdavoodi1376@gmail.com>
2023-04-04 15:52:28 +0430

    remove create method

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use PedramDavoodi\Localization\Controllers\LanguageController;
 use PedramDavoodi\Localization\Controllers\PhraseController;
 
 Route::group(['prefix' => 'localization'  , 'middleware' => 'web'] , function (){
-    Route::resource('language' , LanguageController::class , ['except' => ['show']]);
+    Route::resource('language' , LanguageController::class , ['except' => ['show' , 'create']]);
 
-    Route::resource('/phrase' , PhraseController::class , ['except' => ['index' , 'show' , 'edit']]);
+    Route::resource('/phrase' , PhraseController::class , ['except' => ['index' , 'show' , 'edit' , 'create']]);
 });
